2ca60dbe09d279a3c7d68ea7414f95bd03d3d396
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2019-03-26 09:56:45 +0100

    twitter

---
+++ ejemplotwitter/classpathTwitter.java
@@ -0,0 +1,40 @@
+package ejemplotwitter;
+
+import java.util.List;
+import javax.swing.JOptionPane;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+public class classpathTwitter {
+
+    public void tuit() throws TwitterException {
+        Twitter twitter = TwitterFactory.getSingleton();
+        String latestStatus = JOptionPane.showInputDialog("Introduce texto: ");
+        Status status = twitter.updateStatus(latestStatus);
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
+    }
+
+    public void traerLineaTiempo() throws TwitterException {
+        Twitter twitter = TwitterFactory.getSingleton();
+        List<Status> statuses = twitter.getHomeTimeline();
+        System.out.println("Showing home timeline.");
+        for (Status status : statuses) {
+            System.out.println(status.getUser().getName() + ":"
+                    + status.getText());
+        }
+    }
+
+    public void filtrado() throws TwitterException {
+        Twitter twitter = TwitterFactory.getSingleton();
+        Query query = new Query("source:twitter4j yusukey");
+        QueryResult result = twitter.search(query);
+        for (Status status : result.getTweets()) {
+            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+        }
+    }
+
+}
